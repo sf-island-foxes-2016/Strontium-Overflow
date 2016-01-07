9887489adce9fa66bfcc5dd9b6dde2dbda88ee0e
Jon Clayton <Jonclayton@swbell.net>
2016-01-07 15:13:48 -0800

    Finish  controllers subject to debugging

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,4 +1,7 @@
 post '/votes' do #post perform create or replace vote (no point in update since single field)
-
+  @vote = Vote.new(params[:answer])
+  @vote.user_id = session[:user_id]
+  @vote.save
+  redirect "/questions/:#{@vote.question_id}"
 end
 
