b6a693d50f8d4286ec6568747220d7d5da86a2f8
Jon Clayton <Jonclayton@swbell.net>
2016-01-07 21:55:41 -0800

    Add jon's front end updates

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,12 +1,18 @@
-get '/answers/new' do #get create form
+get '/answers/new/question/:id' do #get create form
+  @question = Question.find(params[:id])
   erb :'answer/create'
 end
 
 post '/answers' do #post create form to perform create
   @answer = Answer.new(params[:answer])
   @answer.user_id = session[:user_id]
+  @answer.question_id = @question.id
   @answer.save
-  redirect "/questions/:#{answer.question_id}"
+  puts "Answer contents is #{@answer.inspect}"
+  puts "AQID is #{@answer["question_id"]}"
+  puts "AQ is #{@answer["question"]}"
+  puts "AQQID is #{@answer.question.question_id}"
+  redirect "/questions/#{@answer.question_id}"
 end
 
 get '/answers/:id/edit' do # get update form
@@ -17,7 +23,7 @@ end
 put '/answers/:id' do # put update form to perform update
   @answer = Answer.find(params[:id])
   @answer.update_attributes(params[:answer])
-  redirect "/questions/:#{answer.question_id}"
+  redirect "/questions/#{@answer.question_id}"
 end
 
 
--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -2,6 +2,6 @@ post '/votes' do #post perform create or replace vote (no point in update since 
   @vote = Vote.new(params[:answer])
   @vote.user_id = session[:user_id]
   @vote.save
-  redirect "/questions/:#{@vote.question_id}"
+  redirect "/questions/#{@vote.question_id}"
 end
 
