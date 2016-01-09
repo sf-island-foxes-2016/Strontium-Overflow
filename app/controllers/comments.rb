19933010972a4ee3cc7e2bdb97d631582d629df2
Jon Clayton <Jonclayton@swbell.net>
2016-01-08 16:19:53 -0800

    Add comments and answers working

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -3,11 +3,18 @@ get '/comments/new/question/:id' do #get create form
   erb :'comment/new_to_question'
 end
 
+get '/comments/new/answer/:id' do #get create form
+  @answer = Answer.find(params[:id])
+  erb :'comment/new_to_answer'
+end
+
 post '/comments' do #post create form to perform create
   @comment = Comment.new(params[:comment])
   @comment.user_id = session[:user_id]
   @comment.save
-  redirect "/questions/#{@comment.question_id}"
+  redirect "/questions/#{@comment.commentable_id}" if @comment.commentable_type == "Question"
+  puts @comment.inspect
+  redirect "/questions/#{Answer.find(@comment.commentable_id).question_id}"
 end
 
 get '/comments/:id/edit' do # get update form
